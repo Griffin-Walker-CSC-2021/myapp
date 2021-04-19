4815c898085635103d95f0daccf4bd21c482b2b4
hunterawalker <71032787+hunterawalker@users.noreply.github.com>
2021-04-18 20:33:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-   <head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewponit" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -8,61 +8,65 @@ function App() {
     <link rel="stylesheet" href="style.css">
 </head>
 <header>
-<div class="topnav">
+<div className="topnav">
     <img src:C:\Users\hunte\Desktop\SIminar project files\thumbnail_WP_vertical_main.png>
-    <a class="active" href="http://www.wolfprairie.com/">Home</a>
+    <a className="active" href="http://www.wolfprairie.com/">Home</a>
     <a href="http://www.wolfprairie.com/wolf-prairie-institute">Real-Life Learning</a>
     <a href="http://www.wolfprairie.com/#ContactUs">Contact Us</a>
     <a href="http://www.wolfprairie.com/#AboutUs">About Us</a>
   </div> 
 </header>
 <body> 
-    <div class="banner-area">
-        <div class="content-area">
-                <div class="content">
+    <div className="banner-area">
+        <div className="content-area">
+                <div className="content">
                     <h1>Invisible Influences</h1>
                     <p>Learn where you have Biases!</p>
                 </div>
         </div>
     </div>
-       <div class="container">
-           <div class="box">
+       <div className="container">
+           <div className="box">
                <p>
                 This is a test to see if the boxes work and if they look good.
                </p>
            </div>
        </div>
 
-       <div class="container">
-        <div class="box">
+       <div className="container">
+        <div className="box">
             <p>
              This is a test to see if the boxes work and if they look good.
             </p>
         </div>
     </div>
 
-    <div class="container">
-        <div class="box">
+    <div className="container">
+        <div className="box">
             <p>
              This is a test to see if the boxes work and if they look good.
             </p>
         </div>
     </div>
 
-    <div class="container">
-        <div class="box">
+    <div className="container">
+        <div className="box">
             <p>
              This is a test to see if the boxes work and if they look good.
             </p>
         </div>
     </div>
-    <div class = "buttoncenter">
-    <a href="questionniare.html" class="button">Take Quiz</a></a>
+    <div className = "buttoncenter">
+    <a href="questionniare.html" className="button">Take Quiz</a></a>
 </div>
 </body>
-        </a>
-      </header>
-    </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+    
   );
 }
 
