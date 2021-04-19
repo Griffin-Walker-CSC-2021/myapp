ec5baa89ec2a7021d25145a8deccbfb647cd7e14
hunterawalker <71032787+hunterawalker@users.noreply.github.com>
2021-04-18 19:34:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,73 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewponit" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <Title>Workplace Biases</Title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<header>
+<div className="topnav">
+    <img src:C:\Users\hunte\Desktop\SIminar project files\thumbnail_WP_vertical_main.png>
+    <a className="active" href="http://www.wolfprairie.com/">Home</a>
+    <a href="http://www.wolfprairie.com/wolf-prairie-institute">Real-Life Learning</a>
+    <a href="http://www.wolfprairie.com/#ContactUs">Contact Us</a>
+    <a href="http://www.wolfprairie.com/#AboutUs">About Us</a>
+  </div> 
+</header>
+<body> 
+    <div className="banner-area">
+        <div className="content-area">
+                <div className="content">
+                    <h1>Invisible Influences</h1>
+                    <p>Learn where you have Biases!</p>
+                </div>
+        </div>
+    </div>
+       <div className="container">
+           <div className="box">
+               <p>
+                This is a test to see if the boxes work and if they look good.
+               </p>
+           </div>
+       </div>
+
+       <div className="container">
+        <div className="box">
+            <p>
+             This is a test to see if the boxes work and if they look good.
+            </p>
+        </div>
+    </div>
+
+    <div className="container">
+        <div className="box">
+            <p>
+             This is a test to see if the boxes work and if they look good.
+            </p>
+        </div>
+    </div>
+
+    <div className="container">
+        <div className="box">
+            <p>
+             This is a test to see if the boxes work and if they look good.
+            </p>
+        </div>
+    </div>
+    <div className = "buttoncenter">
+    <a href="questionniare.html" className="button">Take Quiz</a></a>
+</div>
+</body>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
